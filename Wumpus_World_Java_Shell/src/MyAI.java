fae751746ba2a49518c9990d96c45cc7b3e52664
Alex S. Park <parka4@andromeda-39.ics.uci.edu>
2018-10-29 19:34:38 -0700

    updatePath

--- Wumpus_World_Java_Shell/src/MyAI.java
+++ Wumpus_World_Java_Shell/src/MyAI.java
@@ -16,45 +16,82 @@
 //                the code. Any changes to other portions of the code will
 //                be lost when the tournament runs your code.
 // ======================================================================
+import java.util.ArrayList;
+import java.lang.Object;
+import java.awt.*;
+
 
 public class MyAI extends Agent
 {
+	private ArrayList<Point> coordinates = new ArrayList<Point>();
+	private enum Direction {NORTH, EAST, SOUTH, WEST}
+	private Direction agent_direction;
 	public MyAI ( )
 	{
-		// ======================================================================
-		// YOUR CODE BEGINS
-		// ======================================================================
-		
-		// ======================================================================
-		// YOUR CODE ENDS
-		// ======================================================================
+		coordinates.add(new Point(0,0));
+		agent_direction = Direction.EAST;
+
 	}
 	
 	public Action getAction
-	(
-		boolean stench,
-		boolean breeze,
-		boolean glitter,
-		boolean bump,
-		boolean scream
-	)
+	(boolean stench,
+	boolean breeze,
+	boolean glitter,
+	boolean bump,
+	boolean scream)
 	{
-		// ======================================================================
-		// YOUR CODE BEGINS
-		// ======================================================================
-		
+		Point origin = new Point(0,0);
+		if((stench || breeze) && coordinates.get(coordinates.size()-1) ==  origin)
+		{
+			return Action.CLIMB;
+		}
+		updatePath();
 		return Action.CLIMB;
-		// ======================================================================
-		// YOUR CODE ENDS
-		// ======================================================================
+		if(stench || breeze && coordinates.get(coordinates.size()-1) == origin)
+		{
+		    return Action.CLIMB;
+		}
+
+		//goBack functior
+		//if(stench || breeze) go back
+		//try different path than before
+		//keep track of different direction function
+		
+		//updatePath();
+		//return Action.FORWARD;
+
+		if(coordinates.get(coordinates.size() - 1) == origin)
+		{
+		    //have to make getGold function
+		    return Action.CLIMB;
+		}
+
+
 	}
 	
-	// ======================================================================
-	// YOUR CODE BEGINS
-	// ======================================================================
+	public void updatePath()
+	{
+	    Point current_position = coordinates.get(coordinates.size()-1);
+	    int x = current_position.getX();
+	    int y = current_position.getY();
+	    if(agent_direction == Direction.NORTH && bump == false){
 
+		x += 1;
+	    }
+	    else if(agent_direction == Direction.EAST && bump == false){
+		y += 1;
+	    }
+	    else if(agent_direction == Direction.SOUTH && !inFrontOfWall(current_position)){
+		x -= 1;
+	    }
+	    else if(agent_direction == Direction.WEST && !inFrontOfWall(current_position)){
+		y -= 1;
+	    }
+	    coordinates.add(new Point(x,y));
+	}
 
-	// ======================================================================
-	// YOUR CODE ENDS
-	// ======================================================================
+	public void goBack()
+	{
+	}
+
 }
